66784d442ebbe75291a3671d0a059b9990c36fc8
nazish iqbal <nazish.iqbal@thankyo.net>
2023-08-29 20:09:42 +0500

    second commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web gunicorn NetkErp.wsgi:application --log-file -
+web gunicorn parashooterp.wsgi:application --log-file -
--- parashooterp/wsgi.py
+++ parashooterp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NetkErp project.
+WSGI config for parashooterp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
